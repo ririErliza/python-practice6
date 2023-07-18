ab2963be971d6deaa190de939ed04ad6e18d7724
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-18 09:56:11 +0200

    Update matplotlib_l.py

--- matplotlib_l.py
+++ matplotlib_l.py
@@ -95,8 +95,48 @@ plt.title("Sport Watch Data", loc= 'left')
 plt.xlabel("Average Pulse")
 plt.ylabel("Calorie Burnage")
 
-plt.plot(x,y)
-plt.show()
+# plt.plot(x,y)
+# plt.show()
 
 
 '''--------------------   Grid   --------------------------'''
+
+x = np.array([76, 83, 90, 95, 100, 105, 110, 115, 120, 125])
+y = np.array([240, 250, 260, 270, 280, 290, 300, 310, 320, 330])
+
+plt.title("Sport Watch Data", loc= 'left')
+plt.xlabel("Average Pulse")
+plt.ylabel("Calorie Burnage")
+
+# plt.plot(x,y, color = 'r')
+
+# plt.grid(color='g', ls='--', linewidth = 0.4)
+# plt.show()
+
+'''--------------------   Subplot   --------------------------'''
+
+#plot 1
+x = np.array([0, 1, 2, 3])
+y = np.array([3, 8, 1, 10])
+
+plt.subplot(1,2,1)
+plt.plot(x,y)
+plt.title("SALES")
+
+#plot 2
+x = np.array([0, 1, 2, 3])
+y = np.array([10, 20, 30, 40])
+
+plt.subplot(1,2,2)
+plt.plot(x,y)
+plt.title("INCOME")
+
+plt.suptitle("MY SHOP")
+plt.show()
+
+# plt.subplot(1, 2, 1)
+# #the figure has 1 row, 2 columns, and this plot is the first plot.
+
+# plt.subplot(1, 2, 2)
+# #the figure has 1 row, 2 columns, and this plot is the second plot.
+
