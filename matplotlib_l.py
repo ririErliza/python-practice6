d47105780e9f631525dbbbfd491a2e90722c8f28
Erliza Erbaryanti <95483807+ririErliza@users.noreply.github.com>
2023-07-18 09:38:03 +0200

    Update matplotlib_l.py

--- matplotlib_l.py
+++ matplotlib_l.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+
 '''------------------ -   Plotting   --------------------------'''
 
 ''' Plotting x and y points '''
@@ -27,9 +28,37 @@ y = np.array([3,10])
 x = np.array([1,2,6,8])
 y = np.array([3,8,1,10])
 
-plt.plot(x,y)
-plt.show()
+# plt.plot(x,y)
+# plt.show()
 
 
 ''' Default X-Points '''
 
+ypoints = np.array([3,8,1,10,5,7])
+
+# plt.plot(ypoints)
+# plt.show()
+
+
+'''--------------------   Marker   --------------------------'''
+
+vAxis = np.array([3,8,1,10])
+
+plt.plot(vAxis, marker='o', ms=20, mec= 'hotpink', mfc='hotpink')
+plt.show()
+
+# ms = markersize
+# mec = markeredgecolor
+# mfc = markerfacecolor
+
+'''--------------------   Line   --------------------------'''
+#single line
+
+ypoints = np.array([3,8,1,10])
+# plt.plot(ypoints, linestyle = 'dotted')
+plt.plot(ypoints, ls = ':', color = 'r')
+plt.show()
+
+# linestyle can be written as ls.
+# dotted can be written as :.
+# dashed can be written as --.
